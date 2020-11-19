c4873b5ea849a4cd84775e4c5875ce1fb1f56c0c
RASHMI KARNANY <41475821+RASHMI-KARNANY@users.noreply.github.com>
2020-11-19 12:54:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use('/api/users', require("./routes/api/users"));
+
+app.listen(6000, () => {
+    console.log('server started')
+})
+
---
+++ user.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: 'Rashmi',
+        age: 19,
+    },
+    {
+        id: 2,
+        name: 'Harshil',
+        age: 20, 
+    },
+    {
+        id: 3,
+        name: 'Brownie',
+        age: 1,
+    },
+    {
+        id: 4,
+        name: 'puchu',
+        age: 5,
+    }
+];
+
+module.exports = users;
